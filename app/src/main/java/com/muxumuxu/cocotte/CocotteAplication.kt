a214f78d025b4fb11e001093436acd32fd2f9ab5
Jimmy Mayoukou <bhullnatik@gmail.com>
2017-08-28 01:47:30 +0200

    Add Crashlytics

--- app/src/main/java/com/muxumuxu/cocotte/CocotteAplication.kt
+++ app/src/main/java/com/muxumuxu/cocotte/CocotteAplication.kt
@@ -1,13 +1,17 @@
 package com.muxumuxu.cocotte
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.muxumuxu.cocotte.analytics.AmplitudeTracker
 import com.muxumuxu.cocotte.analytics.Analytics
 import com.muxumuxu.cocotte.analytics.MixpanelTracker
+import io.fabric.sdk.android.Fabric
 
 class CocotteAplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Fabric.with(this, Crashlytics())
 
         Analytics.init(AmplitudeTracker(this), MixpanelTracker(this))
     }
